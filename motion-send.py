db42369335b2662671b70dacb973dbc6f7657d6b
Jack Fitton <93110524+jackfitton112@users.noreply.github.com>
2023-07-09 22:32:30 +0100

    refactored motion-send.py

--- motion-send.py
+++ motion-send.py
@@ -1,58 +1,96 @@
-import os
+import os, time, json, base64, asyncio, aiohttp
 import paho.mqtt.client as mqtt
-import time
-import json
-import base64
 from dotenv import load_dotenv
-import requests
 
-load_dotenv()
+def loadFromEnv():
 
+    #load the .env file using the dotenv library
+    if load_dotenv == False:
+        assert False, "Error loading .env file"
 
-# MQTT settings
-try:
-    MQTT_BROKER = os.getenv("MQTT_BROKER")
-    MQTT_PORT = int(os.getenv("MQTT_PORT"))
+    #declare the global variables to be used in the script
+    global MQTT_BROKER
+    global MQTT_PORT
+    global MOTION_EYE_URL
+    global MQTT_USER
+    global MQTT_PASS
+
+    MQTT_BROKER = os.getenv("MQTT_BROKER") 
+    MQTT_PORT = int(os.getenv("MQTT_PORT")) #Must be type cast to int for lib
     MOTION_EYE_URL = os.getenv("MOTION_EYE_URL")
-except:
-    print("Error getting environment variables")
-    exit(1)
+    MQTT_USER = os.getenv("MQTT_USER")
+    MQTT_PASS = os.getenv("MQTT_PASS")
 
-# TODO: add optional auth for mqtt
+    #check if the variables are set
+    if (MQTT_BROKER == None or MQTT_PORT == None or MOTION_EYE_URL == None or 
+        MQTT_USER == None or MQTT_PASS == None):
+
+        assert False, "Error loading .env variables"
+
+    return True
+
+#load the variables from the .env file
+loadFromEnv()
+
+# Global variables, don't change these values
+CAMERA_NAME = "front-door"
+IMAGE_DIR_PATH = ""
 MQTT_KEEPALIVE = 60
 MQTT_TOPIC = "home/doorbell/motion"
 MQTT_SUB_TOPIC = "home/doorbell/live"
-#MQTT_USERNAME = "mqtt"
-#MQTT_PASSWORD = "mqtt"
 
-    # Create the MQTT client
-client = mqtt.Client()
+
+# Create the MQTT client from the paho library 
+# (https://github.com/eclipse/paho.mqtt.python)
+
+MQTT_CLEAN_SESSION = True
+MQTT_TRANSPORT = "tcp"
+MQTT_RECONNECT_ON_NETWORK_LOSS = True
+MQTT_RECONNECT_DELAY_SECS = 2
+MQTT_PROTOCOL = mqtt.MQTTv5
+
+# Create the MQTT client from the paho library using the settings above
+
+client = mqtt.Client(clean_session=MQTT_CLEAN_SESSION, 
+                     transport=MQTT_TRANSPORT, 
+                     protocol=MQTT_PROTOCOL,
+                     reconnect_delay=MQTT_RECONNECT_DELAY_SECS,
+                     reconnect_delay_max=60,
+                     reconnect_on_failure=MQTT_RECONNECT_ON_NETWORK_LOSS
+                     )
+
+
      
-    # Connect to the MQTT broker
+# Connect to the MQTT broker using the settings above
 client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
 
 
 # This function gets called when the MQTT client is connected to the broker
-def on_connect(client, userdata, flags, rc):
+async def on_connect(client, userdata, flags, rc):
     #if connection successful rc = 0
     if rc == 0:
         #subscribes to the "home/doorbell/live" topic
         client.subscribe(MQTT_SUB_TOPIC)
     else:
+        #TODO: add logging / error handling
+        # should attempt to reconnect 3 times then exit
         print("Connection failed")
 
 # This function gets called every time a message is received from the MQTT broker on the subscribed topic
-def on_message(client, userdata, msg):
+async def on_message(client, userdata, msg):
 
-    
-    
     # Get the message from the MQTT broker
     message = msg.payload.decode("utf-8")
 
     # If the message is "true" then get the images
     if message == "true":
         # Get the images from the motion eye server
-        get_images_http()
+        await get_images_http()
+
+    else:
+        # Return false as only interested in "true" messages
+        # doesnt require a return but added for clarity
+        return False
 
 
 
@@ -60,41 +98,53 @@ def on_message(client, userdata, msg):
 # Setup the callbacks for the MQTT client
 client.on_connect = on_connect
 client.on_message = on_message
-     
 
-# Script settings - TODO: move to env / ToF
-CAMERA_NAME = "front-door"
-IMAGE_DIR_PATH = ""
 
-def get_images():
+# This is a function to capture the images from the motion eye server and return them as a base64 string
+# it either returns a list of images or false if there are no images
+def get_images() -> list / bool:
     # Get the list of images
     images = []
-    try:
-        dirs = os.listdir(IMAGE_DIR_PATH)
 
-    except:
-        return False
+    # Check if the directory exists where the images are stored
+    if os.path.isdir(IMAGE_DIR_PATH):
+        # images are stored in sub directories, get the list of sub directories
+        directories = os.listdir(IMAGE_DIR_PATH)
+    else:
+        assert False, "Error: IMAGE_DIR_PATH Directory does not exist"
 
-    if len(dirs) == 0:
+
+    # if the directory is empty or only contains the .keep file then return false
+    if len(directories) == 0 or (len(directories) == 1 and directories[0] == ".keep"):
         return False
+    else:
+        #remove the .keep file from the list
+        directories.remove(".keep")
+
+
 
     # Loop through the files in the directory
-    for directory in dirs:
+    for directory in directories:
 
-        if directory == ".keep":
+        #check the directory is not empty
+        if len(os.listdir(IMAGE_DIR_PATH + directory)) == 0:
             continue
 
-        # Get the list of files in the directory
-        files = os.listdir(IMAGE_DIR_PATH + directory)
+        # Get the list of images in the directory
+        images = os.listdir(IMAGE_DIR_PATH + directory)
 
         # Loop through the files in the directory
-        for file in files:
+        for image in images:
+                
+                #check file is an image
+                if not (image.endswith(".jpg") or image.endswith(".png") or image.endswith(".jpeg")):
+                    continue
                 
                 # Get the file path
-                file_path = IMAGE_DIR_PATH + directory + "/" + file
-                print(file_path)
-    
-                # Open the file
+                file_path = IMAGE_DIR_PATH + directory + "/" + image
+                
+
+                # Open the file and encode it as a base64 string
                 with open(file_path, "rb") as image_file:
     
                     # Encode the file as base64
@@ -106,13 +156,20 @@ def get_images():
                 # Delete the file
                 os.remove(file_path)
 
+        # Delete the directory
         os.rmdir(IMAGE_DIR_PATH + directory)
 
-    # Return the list of images
+    # Return the list of images as a base64 string
     return images
 
-def send_images(images):
+
+#This function sends the images to the MQTT broker as a json payload
+def send_images(images: list) -> bool:
      
+     # Check if the images are a list
+    if not isinstance(images, list):
+        #should never happen but added for safety
+        assert False, "Error: Images must be a list" 
 
     # Loop through the images
     for image in images:
@@ -121,18 +178,21 @@ def send_images(images):
         payload = json.dumps({"time": time.time(), "camera": CAMERA_NAME, "image": image.decode("utf-8")})
      
         # Publish the payload
-        client.publish(MQTT_TOPIC, payload)
+        client.publish(MQTT_TOPIC, payload, qos=2)
      
     # Return true
     return True
 
-def get_images_http():
+# This function gets the images directly
+# from the motion eye server and sends them to the MQTT broker
+# this is used when `live` is requested
+async def get_images_http() -> bool:
 
     #TODO: This is currently blocking, need to make it async
 
     url = MOTION_EYE_URL + "/picture/1/current/"
 
-    res = requests.get(url)
+    res = await aiohttp.get(url)
 
 
     if res.status_code == 200:
@@ -140,12 +200,12 @@ def get_images_http():
         encoded_string = base64.b64encode(res.content)
 
     else:
+        # either the url is wrong or the server is down
+        assert False, "Error: Could not get images from motion eye server, check the url"
 
-        return False
 
 
-
-    #publish the image
+    #publish the image to the mqtt broker on the "home/doorbell/motion" topic
     payload = json.dumps({"time": time.time(), "camera": CAMERA_NAME, "image": encoded_string.decode("utf-8")})
 
     # Publish the payload
@@ -153,22 +213,28 @@ def get_images_http():
 
     return True
 
+async def main() -> None:
+
+    # Connect to the MQTT broker
+    client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
+
+    # Start the MQTT client loop
+    client.loop_start()
+
+    # Run the loop forever
+    while True:
+        #check if images are available
+        images = get_images()
+
+        if images:
+            #send the images to the MQTT broker
+            await send_images(images)
+        else:
+            #wait 1 second
+            await asyncio.sleep(1)
 
 
-    
-
-
-
-#FIXME: all of this below needs fixing, should be async and not blocking
-client.loop_start()
-
-while True:
-     
-    data = get_images()
-
-    if data != False:
-         send_images(data)
-         
-    time.sleep(1)
+# Run the main function
+asyncio.run(main())
           
 
